03ac377e472ea5b05bb9659c4d0e383a4eaccc9d
Alejandro Castro <120747172+AleCastro06-collab@users.noreply.github.com>
2026-07-24 14:34:23 -0500

    Delete CNN3DMultiAtt directory

--- CNN3DMultiAtt/data/__init__.py
+++
@@ -1,3 +0,0 @@
-from .dataset import experiment_name, get_fold_loaders, set_seed
-
-__all__ = ["experiment_name", "get_fold_loaders", "set_seed"]
--- CNN3DMultiAtt/model/__init__.py
+++
@@ -1,3 +0,0 @@
-from .cnn3d_multi_att import CNN3DMultiAtt
-
-__all__ = ["CNN3DMultiAtt"]
